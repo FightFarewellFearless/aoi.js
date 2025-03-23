d01f8d2d643d57cb50c471bb2bf01167245c8052
FightFarewellFearless <faizakbar088@gmail.com>
2025-03-23 13:28:54 +0700

    feat: add delayed shutdown and reboot

--- src/functions/reboot.js
+++ src/functions/reboot.js
@@ -1,8 +1,12 @@
+const { Time } = require("../core/Time");
+
 /**
  * @param {import("..").Data} d
  */
 module.exports = d => {
-    const { code } = d.util.aoiFunc(d);
+    const data = d.util.aoiFunc(d);
+
+    const [rebootAfter = '0'] = data.inside.splits;
 
     try {
         process.on("exit", () => {
@@ -12,8 +16,18 @@ module.exports = d => {
                 stdio: "inherit",
             });
         });
-        process.exit();
     } catch (e) {
         return d.aoiError.fnError(d, 'custom', {}, `Failed To Restart With Reason: ${e}`);
+    }
+
+    if (rebootAfter === '0') {
+        process.exit();
+    } else {
+        const time = isNaN(rebootAfter) ? Time.parse(rebootAfter)?.ms : Number(rebootAfter);
+        setTimeout(process.exit, time);
+    }
+
+    return {
+        code: d.util.setCode(data),
     }
 }
--- src/functions/shutdown.js
+++ src/functions/shutdown.js
@@ -1,7 +1,26 @@
+const { Time } = require("../core/Time");
+
 /**
  * @param {import("..").Data} d
  */
 module.exports = async (d) => {
-    d.client.destroy();
-    process.exit();
+    const data = d.util.aoiFunc(d);
+    
+    const [shutdownAfter = '0'] = data.inside.splits;
+    
+    const shutdown = () => {
+        d.client.destroy();
+        process.exit();
+    }
+
+    if(shutdownAfter === '0') {
+        shutdown();
+    } else {
+        const time = isNaN(shutdownAfter) ? Time.parse(shutdownAfter)?.ms : Number(shutdownAfter);
+        setTimeout(shutdown, time);
+    }
+
+    return {
+        code: d.util.setCode(data),
+    }
 };
