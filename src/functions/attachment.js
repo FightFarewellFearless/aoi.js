a9558c63757c7667ce8c8ce956404c5b6ab565bf
FightFarewellFearless <faizakbar088@gmail.com>
2025-03-25 16:47:22 +0700

    feat: add optional encoding to $attachment

--- src/functions/attachment.js
+++ src/functions/attachment.js
@@ -7,10 +7,10 @@ module.exports = async (d) => {
     const data = d.util.aoiFunc(d);
     if (data.err) return d.error(data.err);
 
-    let [attachment, name, type = "url"] = data.inside.splits;
+    let [attachment, name, type = "url", encoding] = data.inside.splits;
     const result = new AttachmentBuilder(
         type === "buffer"
-            ? Buffer.from(attachment.addBrackets())
+            ? Buffer.from(attachment.addBrackets(), encoding)
             : attachment.addBrackets(),
         {name: name.addBrackets()},
     );
