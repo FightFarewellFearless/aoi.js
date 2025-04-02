8b9fe4abf35d89ade5c6a4f56a49c5df99c6ff9a
Ivaylo <imsamlolll@gmail.com>
2025-04-02 23:10:13 +0300

    Fix allApplicationCommands.js (#701)
    * Update clientPrefixes.js
    
    * Update allApplicationCommands.js

--- src/functions/allApplicationCommands.js
+++ src/functions/allApplicationCommands.js
@@ -3,16 +3,15 @@
  */  
 module.exports = async (d) => {  
     const data = d.util.aoiFunc(d);  
-  
+
     if (data.err) return d.error(data.err);  
-  
+
     let [separator = ','] = data.inside.splits;  
-    try {  
-        const slashCommands = await client.application.commands.fetch();  
-        const formattedSlashCommands = slashCommands.map(command => command.name).join(` ${separator} `)  
-   } catch (e) {  
-         return d.aoiError.fnError(d, 'custom', {}, `Failed to fetch all slash commands with the reason: ${e}`);  
-    }  
+    const slashCommands = await d.client.application.commands.fetch().catch((e) => {
+         d.aoiError.fnError(d, 'custom', {}, 'Failed To Fetch Slash Commands With The Reason: ' + e);  
+         });  
+    data.result = slashCommands.map(command => command.name).join(separator);
+
     return {  
         code: d.util.setCode(data),  
     };  
